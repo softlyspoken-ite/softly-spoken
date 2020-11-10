6a81f93c1fcd588c52c2e969ed880b39032efdd4
Nuttachai20 <teaynekd@gmail.com>
2020-11-11 03:36:12 +0700

    edit&changepassword

--- softlyspoken/routes/index.js
+++ softlyspoken/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('editprofile', { title: 'Express' });
+    res.render('changepassword', { title: 'Express' });
 });
 
 module.exports = router;
