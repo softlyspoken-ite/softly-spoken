0ab8a8ad8aba6d0064ceab5ecd0a76dd43515e92
Nuttachai20 <teaynekd@gmail.com>
2020-11-19 13:57:56 +0700

    updateLogin

--- softlyspoken/routes/index.js
+++ softlyspoken/routes/index.js
@@ -3,11 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
-    res.render('index', { title: 'Express' });
-=======
-    res.render('profile', { title: 'Express' });
->>>>>>> 0bfe8d4a4ac4cb44cb068122bc4fdd7af84a72cd
+    res.render('login', { title: 'Express' });
 });
 
 module.exports = router;
